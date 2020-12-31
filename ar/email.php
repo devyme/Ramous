9b0acc94c1bc916fe0a374fa967d7e3619ccb930
El Mehdi Azgaou <medevy@protonmail.com>
2020-12-31 17:27:04 +0100

    Uploaded

---
+++ ar/email.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$adresse = $_POST['adresse'];
+$request = $_POST['request'];
+
+$to = "f7k6iu0@aroli.ma, arolivraison@gmail.com";
+$subject = "$name";
+$body = "-----------------Don't reply to this message!----------------- \n\nNom : $name \n\n E-mail : $email \n\n Telephone : $phone \n\n Adresse : $adresse \n\n Demande : $request";
+
+mail($to, $subject, $body);
+
+header("Location: /ar");
+
+?>
+
+
